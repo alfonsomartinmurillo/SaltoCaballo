23a906e7b71db492515d870fb28fecbb30a01be6
Alfonso Martín Murillo <alfonsomartinmurillo@gmail.com>
2012-12-19 13:07:20 +0100

    Version 1.0
    He depurado, ahora obtengo de inmediato 5x5, 6x6, 7x7. 8x8 todavía no.

--- src/EstadoPartida.java
+++ src/EstadoPartida.java
@@ -20,9 +20,13 @@ public class EstadoPartida
 	public boolean modoTraza;
 	public boolean modotodasSoluciones;
 	public int dimTablero;
+	public int dimTableroidx0;
 	public boolean esSolucion;
 	public  PrintStream miConsola;
-	public int numPosValidas;
+	public int xDesp[];
+	public int numSoluciones;
+	public int yDesp[];
+	public long numLlamadas;
 	
 	
 	//CONSTRUCTOR POR DEFECTO.
@@ -65,17 +69,7 @@ public class EstadoPartida
 		//Establezco la primera posición del caballo en el tablero
 		Tablero[xActual][yActual]=1;
 		dimTablero=dim; //Establezco la dimensión del Tablero
-		Camino=new Casilla[dim*dim]; //Genero un nuevo array de tipo Casilla
-		//Crear todas las casillas del array Camino.
-		for (idx=0;idx<(dim*dim);idx++)
-		{
-			Camino[idx]=new Casilla(); //Genero la lista de casillas inicial
-		}
 		
-		// Inicializo la primera posición del camino actual
-		Camino[0].xPos=xIni-1; //LO ESTABLEZCO SOBRE INDICE 0, NO ÍNDICE 1
-		Camino[0].yPos=yIni-1;
-		//Inicializo la longitud inicial del camino
 		lCamino=1;
 		//llamada al constructor por defecto
 		try
@@ -101,104 +95,115 @@ public class EstadoPartida
 	{
 		//Función que focaliza todo lo común a los diferentes constructores
 		esSolucion=false;
+		numSoluciones=0;
+		xDesp=new int[8];
+		yDesp=new int[8];
+		xDesp[0]=-2;xDesp[1]=-1;xDesp[2]=-2;xDesp[3]=-1;xDesp[4]=2;xDesp[5]=1;xDesp[6]=2;xDesp[7]=1;
+		yDesp[0]=-1;yDesp[1]=-2;yDesp[2]=1;yDesp[3]=2;yDesp[4]=-1;yDesp[5]=-2;yDesp[6]=1;yDesp[7]=2;
+		dimTableroidx0=dimTablero-1;
+		numLlamadas=0;
+		
 	}
 	
-	//TODO Limpiar y Comentar
 	public EstadoPartida evalPartida()
 	{
-		
-		//VARIABLES LOCALES
 		int x;
+		int xdesptemp;
+		int ydesptemp;
+		int xActualAnt;
+		int yActualAnt;
 		
-		//Salida en caso de camino válido. Si el camino obtenido corresponde con el tamaño del tablero, es que hemos
-		//ocupado todas las posiciones.
-		//SALIDA VÁLIDA
+		numLlamadas++;
+		if ((numLlamadas % 100000000)==0)
+		{
+			miConsola.println("Número de llamadas: " + Long.toString(numLlamadas) );
+			miConsola.println("Longitud actual: " + Integer.toString(lCamino));
+		}
+		//SI ESTOY EN MODO TRAZA SIEMPRE IMPRIMO TABLERO, INDEPENDIENTEMENTE DE QUE ME ENCUENTRE CON UNA SOLUCIÓN O NO
+		if (modoTraza)
+		{
+			miConsola.println("------------------------------------------------------------------");
+			PintarTablero();
+			miConsola.println();
+		}
+		
+		
+		//ENCONTRAMOS UNA SOLUCIÓN
 		if (lCamino==dimTablero*dimTablero)
+		{
+			numSoluciones++;
+			miConsola.println("Solución encontrada número: "+ Integer.toString(numSoluciones));
+			//IMPRIMO EL TABLERO ÚNICAMENTE SI NO ESTOY EN MODO TRAZA, YA QUE SI NO YA LO TENDRÍA EN PANTALLA
+			if (modoTraza==false)
 			{
-				esSolucion=true;
-				//AÑADO LA SOLUCIÓN A LA LISTA DE SOLUCIONES
-				SaltoCaballo.listaSoluciones.add(this);
-				Date fechaHora=new Date(); 
-				miConsola.println(fechaHora.toString() + " Solución - " + Integer.toString(SaltoCaballo.listaSoluciones.size()));
-				miConsola.println("----------------------------------------------------------");
-					
-				if (modoTraza)
-				{
-					miConsola.println(fechaHora.toString() +  " Detalle de la traza");
-					miConsola.println("----------------------------------------------------------");
-					SaltoCaballo.PintarSolucion(this);
-				}
-				else
-				{
-					PintarTablero();
-					miConsola.println();
-				}
-					miConsola.println();
-
-				
-				
+				miConsola.println("Número de soluciones encontradas: "+ Integer.toString(numSoluciones));
+				miConsola.println("------------------------------------------------------------------");
+				PintarTablero();
+				miConsola.println();
 			}
-		else
-			//OBTENCIÓN DE SIGUIENTES POSICIONES
-			//
-			
-		{
-			
-				//Array para la obtención de las siguientes posiciones disponibles
-				ArrayList<EstadoPartida> ListaEstadosSiguientes;
-				ListaEstadosSiguientes=SaltoCaballo.genCompleciones(this);
-				x=0;
-				while (x<ListaEstadosSiguientes.size()) //realizamos este recorrido mientras tenga ensayos que cumplan
-				{
-					ListaEstadosSiguientes.get(x).evalPartida();
-					
-					if (SaltoCaballo.listaSoluciones.size()==1  && modotodasSoluciones==false) break;// SI HE ENCONTRADO UNA SOLUCIÓN ROMPO EL BUCLE PARA SALIR LO ANTES POSIBLE
-					x++;
-					
-				}
-				//YA HE ACABADO CON MI LISTA DE ESTADOS, ASÍ QUE ME LOS CEPILLO.
-				ListaEstadosSiguientes=null;
-		
-			
-			
-		}
-			return this;
-	
-	}
-	
-	//TODO Limpiar y Comentar
-	public Casilla[] copiaCamino()
-	{
-		int x;
-		Casilla tCamino[];
-		tCamino=new Casilla[dimTablero*dimTablero];
-		for (x=0;x<(dimTablero*dimTablero);x++)
-		{
-			tCamino[x]=new Casilla(Camino[x].xPos, Camino[x].yPos);
-	
-		}
-			return tCamino;
-	}
-	
-	//MÉTODO QUE REPLICA EL TABLERO
-	public int[][] copiaTablero()
-	{
-		//variables temporales;
-				int x,y;
-				int tTablero[][];
-				tTablero=new int[dimTablero][dimTablero];
-				for (x=0;x<dimTablero;x++) //lo recorro al revés para representar que el eje y crece de abajo a arriba y no de arriba a abajo 
-				{
-					for (y=0;y<dimTablero;y++)
-					{
-						tTablero[x][y]=Tablero[x][y];
 						
-					}
-					
-				}
-				return tTablero;
-	}
+			esSolucion=true;
+		}
+		//OBTENCIÓN DE NUEVAS SOLUCIONES
+		else
+		{
+			for (x=0;x<8;x++)
+			{
+				//COMPROBAR SI HAY QUE SEGUIR GENERANDO
+				//GENERAREMOS SIEMPRE QUE, O BIEN NO HAYAMOS ENCONTRADO UNA SOLUCIÓN, O BIEN TENGAMOS QUE ENCONTRAR
+				//TODAS LAS SOLUCIONES
+				if (((modotodasSoluciones==false) && (esSolucion==true))==false)
+				{
+					xdesptemp=xActual+xDesp[x];
+					ydesptemp=yActual+yDesp[x];
+					//COMPROBAR QUE LA NUEVA POSICIÓN ESTÁ EN RANGO Y QUE ES UNA CASILLA NO VISITADA
+					//if ((xdesptemp>=0) && (xdesptemp<=dimTableroidx0) && (ydesptemp>=0) && (ydesptemp<=dimTableroidx0))
+					if (xdesptemp>=0)
+					{
+						if (xdesptemp<=dimTableroidx0)
+						{
+							if (ydesptemp>=0)
+							{
+								if (ydesptemp<=dimTableroidx0)
+								{
+									//COMPROBAR QUE LA CASILLA NO HA SIDO VISITADA AÚN
+									if (Tablero[xdesptemp][ydesptemp]==0)
+									{
+										//TENGO UNA CASILLA VÁLIDA
+										//ACTUALIZO TABLERO Y BAJO EN PROFUNDIDAD
+										xActualAnt=xActual;
+										yActualAnt=yActual;
+										xActual=xdesptemp;
+										yActual=ydesptemp;
+										lCamino++;
+										Tablero[xActual][yActual]=lCamino;
+										
+										//BAJO EN PROFUNDIDAD
+										evalPartida(); 
+										//VUELVO DE ABAJO
+										//TODO PONER EL TABLERO A SU SITUACIÓN ANTERIOR
+										Tablero[xdesptemp][ydesptemp]=0;
+										xActual=xActualAnt;
+										yActual=yActualAnt;
+										lCamino--;
+															
+									} //CIERRE IF
+								} //CIERRE IF
+							} //CIERRE IF
+						} //CIERRE IF
+					} //CIERRE IF
+				} //CIERRE IF
+				
+			} //CIERRE FOR
+			
+		} //CIERRE ELSE
+			
+		
+		
+		return this;
+	} //CIERRE FUNCIÓN
 	
+
 	
 	
 	//TODO Limpiar y Comentar
@@ -206,7 +211,7 @@ public class EstadoPartida
 	{
 		//variables temporales;
 		int x,y;
-		for (x=dimTablero-1;x>=0;x--) //lo recorro al revés para representar que el eje y crece de abajo a arriba y no de arriba a abajo 
+		for (x=dimTableroidx0;x>=0;x--) //lo recorro al revés para representar que el eje y crece de abajo a arriba y no de arriba a abajo 
 		{
 			for (y=0;y<dimTablero;y++)
 			{
@@ -229,19 +234,7 @@ public class EstadoPartida
 		}
 	}
 	
-	//TODO Limpiar y Comentar
-	public void PintarCamino()
-	{
-		int x;
-		for (x=0;x<(dimTablero*dimTablero);x++)
-		{
-			miConsola.println("Posición " + Integer.toString(x+1) + ": x=" + Integer.toString(Camino[x].xPos) + " , y=" + Integer.toString(Camino[x].yPos));
 
-		}
-	}
-	
-
-	
 	
 }
 
--- src/SaltoCaballo.java
+++ src/SaltoCaballo.java
@@ -68,7 +68,7 @@ public class SaltoCaballo {
 				
 				
 		//CREACIÓN DE LA PARTIDA
-		listaSoluciones= new ArrayList<EstadoPartida>();
+		//listaSoluciones= new ArrayList<EstadoPartida>();
 		//COMIENZA MI PARTIDA
 		EstadoPartida estadoInicial=new EstadoPartida(xInicio,yInicio,N,modoTraza,modoTodasSoluciones,miConsola);
 		
@@ -76,76 +76,11 @@ public class SaltoCaballo {
 		estadoInicial.evalPartida();
 		miConsola.println(fechaHora.toString() + " Finalización del Proceso de generación de soluciones");
 		
-		//FINALIZAR EL PROGRAMA SI NO HEMOS ENCONTRADO SOLUCIONES
-		if (listaSoluciones.size()==0)
-		{
-			miConsola.println(fechaHora.toString() + " No se han encontrado soluciones. El programa finalizará su ejecución.");
-			System.exit(0);
 		
-		}
 		
 		
 	}
 	
-	//TODO Limpiar y Comentar
-	public static void PintarSolucion(EstadoPartida solPartida)
-	{
-		
-		//variables temporales;
-		int xcamino,xcaminotmp, x,y;
-		boolean encontrado;
-		
-		//RECORRO TO-DO EL CAMINO QUE REPRESENTA LA SOLUCIÓN
-		for (xcamino=0;xcamino<solPartida.Camino.length;xcamino++)
-		{
-			xcaminotmp=0;
-			miConsola.println("----------------------------------------------------------");
-			
-			for (x=N-1;x>=0;x--) //lo recorro al revés para representar que el eje y crece de abajo a arriba y no de arriba a abajo 
-			{
-				for (y=0;y<N;y++)
-				{
-					encontrado=false;
-					
-					for (xcaminotmp=0;xcaminotmp<=xcamino;xcaminotmp++) //CADA VEZ RECORREMOS EL CAMINO DESDE EL PRINCIPIO HASTA LA POSICIÓN EVALUADA
-					{
-						
-						
-						
-						if (solPartida.Camino[xcaminotmp].xPos==x && solPartida.Camino[xcaminotmp].yPos==y)
-						{
-							encontrado=true;
-							if ((xcaminotmp+1)>9)
-							{
-								miConsola.print(Integer.toString(xcaminotmp+1));
-							}
-							else
-							{
-								miConsola.print(" " + Integer.toString(xcaminotmp+1));
-							}
-							//miConsola.print(Integer.toString(xcamino+1));
-						}
-								
-						
-					}
-					
-					if (encontrado==false) // si no lo he encontrado genero espacio en dicha posición
-					{
-						miConsola.print("  " );
-					}
-					if (y!=N)
-					{
-						miConsola.print("\t");
-						
-					}
-				}
-				
-				miConsola.println(); //Salto de Linea
-			
-			}
-		}
-		
-	}
 	
 	public static Boolean evalParametros(String[] args)
 	{
@@ -227,136 +162,7 @@ public class SaltoCaballo {
 		
 	}
 	
-	//TODO Limpiar y Comentar
-	public static ArrayList<EstadoPartida> genCompleciones(EstadoPartida EstActual)
-	{
 
-		//Variables locales
-		int x,y,z,umbral=8,idxUmbral=0;
-		ArrayList<EstadoPartida> ListaEstadosSiguientes = new ArrayList<EstadoPartida>();
-		ArrayList<EstadoPartida> ListaEstadosSiguientesOrden = new ArrayList<EstadoPartida>();
-		
-		try
-		{
-			//Lista de Estados siguientes
-			
-			x=EstActual.xActual;
-			y=EstActual.yActual;
-			
-			//Realizo la comprobación y llamada de las 8 posibles posiciones 
-			
-			genNuevoJuego(x-2,y-1,ListaEstadosSiguientes,EstActual);
-			genNuevoJuego(x-1,y-2,ListaEstadosSiguientes,EstActual);
-			genNuevoJuego(x-2,y+1,ListaEstadosSiguientes,EstActual);
-			genNuevoJuego(x-1,y+2,ListaEstadosSiguientes,EstActual);
-			genNuevoJuego(x+2,y-1,ListaEstadosSiguientes,EstActual);
-			genNuevoJuego(x+1,y-2,ListaEstadosSiguientes,EstActual);
-			genNuevoJuego(x+2,y+1,ListaEstadosSiguientes,EstActual);
-			genNuevoJuego(x+1,y+2,ListaEstadosSiguientes,EstActual);
-			
-			//ORDENAR LA LISTA DE ESTADOS SIGUIENTES EN FUNCIÓN DEL NÚMERO DE POSICIONES VÁLIDAS
-			while (ListaEstadosSiguientes.size()!=0)
-			{
-				for (z=0;z<ListaEstadosSiguientes.size();z++)
-				{
-					umbral=8;
-					idxUmbral=0;
-					if (ListaEstadosSiguientes.get(z).numPosValidas<=umbral) 
-					{
-						//almaceno el índice y el nuevo umbral
-						umbral=ListaEstadosSiguientes.get(z).numPosValidas;
-						idxUmbral=z;	
-					}
-				}
-				//una vez finaliza el bucle añado el nuevo elemento, que será el menor, y lo borro de la lista original
-				ListaEstadosSiguientesOrden.add(ListaEstadosSiguientes.get(idxUmbral));
-				ListaEstadosSiguientes.remove(idxUmbral);
-				
-			}
-				
-			
-			
-			
-			
-			
-		} //FIN TRY
-		catch (Exception ex)
-		{
-			miConsola.println(ex.toString());
-		}
-		finally
-		{
-			//LIMPIO Y DEVUELVO
-			ListaEstadosSiguientes=null;
-			return ListaEstadosSiguientesOrden; //Temporal
-		}
-		
-		}
-	
-	//TODO Limpiar y Comentar
-	public static void genNuevoJuego(int x,int y,ArrayList<EstadoPartida> ListaEstadosSiguientes, EstadoPartida EstActual)
-	{
-		//Variables Locales
-		EstadoPartida nuevoEstadoPartida;
-		
-		//Comprobar que las nuevas posiciones están dentro del tablero
-		if ((x>=0) && (y>=0) && (x<=EstActual.dimTablero-1) && (y<=EstActual.dimTablero-1) && (EstActual.Tablero[x][y] == 0))  
-		{
-			nuevoEstadoPartida=new EstadoPartida(x+1,y+1,EstActual.dimTablero,modoTraza,modoTodasSoluciones,miConsola);
-			nuevoEstadoPartida.Tablero=EstActual.copiaTablero();
-			nuevoEstadoPartida.Camino=EstActual.copiaCamino();
-			nuevoEstadoPartida.lCamino=EstActual.lCamino+1;
-			nuevoEstadoPartida.Camino[nuevoEstadoPartida.lCamino-1].xPos=x;
-			nuevoEstadoPartida.Camino[nuevoEstadoPartida.lCamino-1].yPos=y;
-			nuevoEstadoPartida.Tablero[x][y]=nuevoEstadoPartida.lCamino;
-			//INTENTO ACORTAR EL NÚMERO DE ESTADOS SIGUIENTES A EVALUAR
-			nuevoEstadoPartida.numPosValidas=getNumPosValidas(nuevoEstadoPartida);
-			//SI NO HAY POSICIONES SIGUIENTES Y NO ESTOY EN UNA SOLUCIÓN AGREGO. 
-			if (((nuevoEstadoPartida.numPosValidas==0) && (nuevoEstadoPartida.lCamino<nuevoEstadoPartida.dimTablero))==false)
-			{
-				ListaEstadosSiguientes.add(nuevoEstadoPartida);
-			}
-			
-				
-			nuevoEstadoPartida=null;
-			
-		}
-		return;
-		
-	}
-	
-	//TODO Limpiar y Comentar
-	public static int getNumPosValidas(EstadoPartida estPartida)
-	
-	{
-		int numPosValidas=0;
-		int x,y;
-		x=estPartida.xActual;
-		y=estPartida.yActual;
-		
-		
-		if ((x-2>=0) && (y-1>=0) && (x-2<=estPartida.dimTablero-1) && (y-1<=estPartida.dimTablero-1) && (estPartida.Tablero[x-2][y-1] == 0)) numPosValidas++;
-		if ((x-1>=0) && (y-2>=0) && (x-1<=estPartida.dimTablero-1) && (y-2<=estPartida.dimTablero-1) && (estPartida.Tablero[x-1][y-2] == 0)) numPosValidas++;
-		if ((x-2>=0) && (y+1>=0) && (x-2<=estPartida.dimTablero-1) && (y+1<=estPartida.dimTablero-1) && (estPartida.Tablero[x-2][y+1] == 0)) numPosValidas++;
-		if ((x-1>=0) && (y+2>=0) && (x-1<=estPartida.dimTablero-1) && (y+2<=estPartida.dimTablero-1) && (estPartida.Tablero[x-1][y+2] == 0)) numPosValidas++;
-		if ((x+2>=0) && (y-1>=0) && (x+2<=estPartida.dimTablero-1) && (y-1<=estPartida.dimTablero-1) && (estPartida.Tablero[x+2][y-1] == 0)) numPosValidas++;
-		if ((x+1>=0) && (y-2>=0) && (x+1<=estPartida.dimTablero-1) && (y-2<=estPartida.dimTablero-1) && (estPartida.Tablero[x+1][y-2] == 0)) numPosValidas++;
-		if ((x+2>=0) && (y+1>=0) && (x+2<=estPartida.dimTablero-1) && (y+1<=estPartida.dimTablero-1) && (estPartida.Tablero[x+2][y+1] == 0)) numPosValidas++;
-		if ((x+1>=0) && (y+2>=0) && (x+1<=estPartida.dimTablero-1) && (y+2<=estPartida.dimTablero-1) && (estPartida.Tablero[x+1][y+2] == 0)) numPosValidas++;
-
-		return numPosValidas;
-	}
-	
-	
-	//TODO Limpiar y Comentar
-	public boolean evalCondicionesPoda()
-	{
-		//
-		return true;
-		
-	}
-
-	
 	
 
 }
